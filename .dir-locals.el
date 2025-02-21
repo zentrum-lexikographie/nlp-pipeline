d1a7d8c2fe1fd4eb0962ce7aab4139eb783af285
Gregor Middell <gregor@middell.net>
2025-02-21 09:22:18 +0100

    chore(Setup): Fix venv setup in Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((python-mode . ((python-test-runner pytest)
-                 (eval . (pyvenv-workon "nlp"))))
+                 (pyvenv-workon "nlp")))
  (clojure-mode . ((cider-preferred-build-tool . clojure-cli)
                   (cider-clojure-cli-aliases . ":build:test:clerk"))))
