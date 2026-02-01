f95b668269660e985cacd0eca81a6e0cfbbb3456
Gregor Middell <gregor@middell.net>
2026-02-01 10:12:01 +0100

    GPT Gateway: minor cleanups

--- gpt-gateway/dev/user.clj
+++ gpt-gateway/dev/user.clj
@@ -3,10 +3,7 @@
    [integrant.repl :refer [go halt reset reset-all]]
    [zdl.gpt-gateway :as gpt-gw]))
 
-(def config
-  (assoc-in gpt-gw/config [::gpt-gw/service :exit-on-cancel?] false))
-
-(integrant.repl/set-prep! (constantly config))
+(integrant.repl/set-prep! (constantly gpt-gw/config))
 
 (comment
   (go)
