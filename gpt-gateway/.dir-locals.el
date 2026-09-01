3e15ae07a50fdd45e2c8c431865b33d735992eb2
Gregor Middell <gregor@middell.net>
2026-09-01 11:29:56 +0200

    Remove deployment-related sources

--- gpt-gateway/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((clojure-mode . ((cider-preferred-build-tool . "clojure-cli")
-                  (cider-clojure-cli-aliases . ":dev:test")
-                  (cider-ns-code-reload-tool . 'clj-reload))))
